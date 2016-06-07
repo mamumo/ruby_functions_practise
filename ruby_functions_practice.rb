17259f8e029628e2337f2c22c3672b123a158189
Marie Moyles <marie.moyles2@mail.dcu.ie>
2016-06-07 16:38:10 +0100

    add add string as number

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -25,3 +25,7 @@ end
 def join_string(string_1,string_2)
   return string_1 + string_2
 end
+
+def add_string_as_number(first_number,second_number)
+  return first_number.to_i + second_number.to_i
+end
