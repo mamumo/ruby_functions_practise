2570b15c735886801ebc9519037f3a37c2e8fcde
Marie Moyles <marie.moyles2@mail.dcu.ie>
2016-06-07 17:10:42 +0100

    add number_to_full_month_name

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -29,3 +29,19 @@ end
 def add_string_as_number(first_number,second_number)
   return first_number.to_i + second_number.to_i
 end
+
+def number_to_full_month_name(x)
+  case x
+  when 1
+    return "January"
+  when 3
+    return "March"
+  when 9
+    return "September"
+  else
+    return "invalid input"
+  end
+end
+
+
+  
